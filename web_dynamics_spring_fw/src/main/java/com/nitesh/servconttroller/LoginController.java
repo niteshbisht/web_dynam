3bd50084518897ae6ee9d9ec2e510767e4a79139
niteshbisht <nitesh_bisht@yahoo.com>
2015-02-18 00:02:40 +0530

    <nitesh.bisht>spring fw controller setup config integration successfull

--- web_dynamics_spring_fw/src/main/java/com/nitesh/servconttroller/LoginController.java
+++ web_dynamics_spring_fw/src/main/java/com/nitesh/servconttroller/LoginController.java
@@ -1,28 +1,24 @@
 package com.nitesh.servconttroller;
 
-import java.io.IOException;
+import java.util.logging.Logger;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Servlet implementation class LoginServlet
  */
+@ComponentScan
 @Controller
-public class LoginController {
-	private static final long serialVersionUID = 1L;
-       
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
+class LoginController {
+	//private static final long serialVersionUID = 1L;
+    Logger lgr = Logger.getLogger("com.nitesh.servconttroller.LoginController");   
+	@RequestMapping("/login")
 	@ResponseBody
 	public String login() {
-		return "redirect:/loginc";
+		lgr.info("Inside Login Controller");
+		return "/loginc";
 	}
 }
