0c11a74c15748911d3986fe8dceedff2a8febc0f
sysxcomintl <sysxcomintl@sysxcom>
2015-10-03 20:32:48 +0530

    "added 3rd angular demo"

---
+++ webdev_kick_start/web_dev_kikstart/WebContent/resources/angular_apps/app3.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+var app3 = angular.module('app3', []);
+
+app3.controller("studentController", function($scope) {
+	$scope.reset = function() {
+		$scope.firstName = "Mahesh";
+		$scope.lastName = "Parashar";
+		$scope.email = "MaheshParashar@tutorialspoint.com";
+	}
+	$scope.reset();
+});
